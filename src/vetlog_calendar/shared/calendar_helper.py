75702acadabd9740484edc19af570c53e9129e46
josdem <joseluis.delacruz@gmail.com>
2026-04-21 09:19:53 -0600

    #26 Adding calendar helper

---
+++ src/vetlog_calendar/shared/calendar_helper.py
@@ -0,0 +1,25 @@
+#  Copyright 2026 Jose Morales contact@josdem.io
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from vetlog_calendar.pets.model import Pet
+from vetlog_calendar.users.model import User
+
+
+class Helper:
+    def __init__(self, pet: Pet, owner: User):
+        self.pet = pet
+        self.owner = owner
+
+    def get_event_title(self) -> str:
+        return f"{self.owner.first_name} - Vaccination appointment for {self.pet.name}"
