25b7b86330f173f61479949cdbf7bb03a26c4a42
josdem <joseluis.delacruz@gmail.com>
2026-04-28 09:07:30 -0600

    #42 Adding locale class

---
+++ src/vetlog_calendar/shared/locale.py
@@ -0,0 +1,26 @@
+#  Copyright 2026 Jose Morales contact@josdem.io
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+class Locale:
+    """Locale class"""
+
+    def __init__(self, language: str = "en"):
+        self.language = language
+
+    def get_event_title(self, owner: str, pet: str) -> str:
+        """Get the event title based on language"""
+        if self.language == "es":
+            return f"{owner} - Cita de vacunación para {pet}"
+        return f"{owner} - Vaccination appointment for {pet}"
