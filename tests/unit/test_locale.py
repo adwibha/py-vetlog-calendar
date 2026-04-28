33c50a514f877915fb69db4a208daca969ff0b2c
josdem <joseluis.delacruz@gmail.com>
2026-04-28 10:57:37 -0600

    #42 Adding locale test

---
+++ tests/unit/test_locale.py
@@ -0,0 +1,29 @@
+#  Copyright 2026 Jose Morales contact@josdem.io
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from vetlog_calendar.shared.locale import Locale
+
+
+def test_return_spanish_title():
+    """Test that the title is returned in Spanish"""
+    locale = Locale(language="es")
+    title = locale.get_event_title(owner="Jose", pet="Sora")
+    assert title == "Jose - Cita de vacunación para Sora"
+
+
+def test_return_english_title():
+    """Test that the title is returned in English"""
+    locale = Locale()
+    title = locale.get_event_title(owner="Jose", pet="Sora")
+    assert title == "Jose - Vaccination appointment for Sora"
